1c0560280ac4fac82ed0bd4bf5cdf02f4d1bd866
kelvin abche <kelvinabache12@gmail.com>
2023-09-09 16:44:13 -0400

    poniendo color al proyecto

--- app.mjs
+++ app.mjs
@@ -1,13 +1,14 @@
 import { createFiles } from "./imports/imports.mjs";
+import pc from"picocolors"
 
 const createFile = createFiles; //llamndo a la funcion iniciadora
 
 createFile.then(() => {
-  console.log("las carpetas se crearon con exicto");
+  console.log(pc.green("las carpetas se crearon con exicto"));
 })
 
 .catch((err) => {
-  if(err.code === 'EEXIST') console.error('ya estas carpetas existen --->',err.path); //si una carpeta existe pasame el error y la direccion de la carpeta
-  else console.error(err) //si hay un error creando una carpeta pasame el error por consola 
+  if(err.code === 'EEXIST') console.error(pc.red('ya estas carpetas existen --->'),pc.underline(err.path)); //si una carpeta existe pasame el error y la direccion de la carpeta
+  else console.error(pc.red(err)) //si hay un error creando una carpeta pasame el error por consola 
 });
 
