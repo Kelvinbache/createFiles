54a503a15aed58246179f2f08ffe1d68358468fd
kelvin abche <kelvinabache12@gmail.com>
2023-09-22 10:30:40 -0400

    haciendo unas modifcaciones a la funcione de errores

--- errors/errors.mjs
+++ errors/errors.mjs
@@ -1,9 +1,13 @@
 export function errors(err) {
-  if (err.code === "ENOENT") {
-    console.error(`las direccion no existe ${err.path}`);
-  } else if (err.code === "EEXIST") {
-    console.error(`este archivo ya existe ${err.path}`);
-  } else {
-    console.error(err);
+  if (err.syscall === "mkdir") {
+  
+  } else if (err.syscall === "writeFile" && err.code !== "EEXIST") {
+    console.error("tenemos un problema creando los archivos", err.path);
+  
+  } else if (err.syscall === "lstat" && err.code !== "ENOENT") {
+    console.error("tenemos un problema eliminando el archivo:", err.path);
+
+  } else if (err.syscall === "scandir" && err.code !== "ENOENT") {
+    console.error("tenemos un problema para recorrer los archivos", err.path);
   }
 }
